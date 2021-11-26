52a5285c28a5a200bad29bc29a3ff8c33998f27b
bryancurtis <curtispro97@gmail.com>
2021-11-26 15:26:05 -0500

    added post info on posts index, going great

--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,18 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 
+from main_app.models import Post
+
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
 
 class Posts(TemplateView):
+    model = Post
     template_name = "posts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
+
