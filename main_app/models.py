bc38d5f141778f00eed5fea4f0359fbdc47efeac
bryancurtis <curtispro97@gmail.com>
2021-11-26 14:57:36 -0500

    post model code created, about to migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
+from typing import Text
 from django.db import models
 
+from django.db.models import Model
+
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField, TextField, IntegerField
+from django.db.models.fields.related import ForeignKey
+
 # Create your models here.
+class Post(Model):
+    title = CharField(max_length=100)
+    image = CharField(max_length=500)
+    description = TextField(max_length=5000)
+    # locationName = CharField(max_length=100)
+    lat = CharField(max_length=9)
+    long = CharField(max_length=9)
+
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return self.title
