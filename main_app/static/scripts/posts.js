7fa0a1ee01fe7f2c131b4dd2fabd182618215755
bryancurtis <curtispro97@gmail.com>
2021-11-28 15:09:41 -0500

    adds refined posts styling and subtle changes, new posts directory in file structure, adds untested signup and login code for future user auth

--- main_app/static/scripts/posts.js
+++ main_app/static/scripts/posts.js
@@ -7,6 +7,11 @@ const modal = function modal() {
     $(".modal").toggle();
 }
 
+const edit = function edit() {
+    $(".edit").toggle();
+}
+
+
 const message = function message() {
     $(".message").hide();
 }
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.db.models import fields
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.urls import reverse
 
@@ -8,7 +8,11 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+# Authentication imports
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # from django.http import request
 from django.contrib import messages
@@ -18,12 +22,22 @@ from django.forms import ModelForm, TextInput
 from main_app.models import Post
 
 # Create your views here.
+
+class IncludeAuthForms:
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["signup_form"] = UserCreationForm()
+    context["login_form"] = AuthenticationForm()
+    return context
+
+
+
 class Home(TemplateView):
     template_name = "home.html"
 
 class Posts(TemplateView):
     model = Post
-    template_name = "posts.html"
+    template_name = "posts/posts.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,20 +49,57 @@ class Posts(TemplateView):
 
 class PostDetail(DetailView):
     model = Post
-    template_name = "post_detail.html"
-
-class PostCreate(CreateView):
-    model = Post
-    fields = ['title', 'image', 'description', 'lat', 'long']
-    template_name = "post_create.html"
-    success_url = "/posts/"
-
-    def get_success_url(self):
-        return reverse('post_detail', kwargs={'pk': self.object.pk})
+    template_name = "posts/post_detail.html"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(PostCreate, self).form_valid(form)
+        return super(PostUpdateForm, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = PostUpdateForm()
+        context["form"] = form
+        return context
+
+class PostCreate(View):
+
+    def post(self, request, pk):
+
+        # probably better if used on update... a little confused because my code doesnt have the same views as exploria with regards to modals and routes
+        form = PostUpdateForm(request.POST)
+        if form.is_valid():
+            Post.objects.filter(pk=pk).update(title=request.POST.get('title'), image=request.POST.get('image'), description=request.POST.get('description'), lat=request.POST.get('lat'), long=request.POST.get('long'))
+            return redirect("posts")
+        else:
+            # idk about this
+            context = {"form": form, "pk": pk}
+            return render(request, "posts/posts.html", context)
+            
+
+    # Modeled after PostUpdate exploria
+    # def post(self, request, pk):
+    #     title = request.POST.get("title")
+    #     image = request.POST.get("img")
+    #     description = request.POST.get('description')
+    #     lat = request.POST.get('lat')
+    #     long = request.POST.get('long')
+
+    #     Post.objects.create(title=title, img=image, description=description, lat=lat, long=long, user=request.user)
+    #     return redirect("post_detail", pk=pk)
+        
+
+# class PostCreate(CreateView):
+#     model = Post
+#     fields = ['title', 'image', 'description', 'lat', 'long']
+#     template_name = "post_create.html"
+#     success_url = "/posts/"
+
+#     def get_success_url(self):
+#         return reverse('post_detail', kwargs={'pk': self.object.pk})
+
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super(PostCreate, self).form_valid(form)
 
 class PostCreateForm(ModelForm):
     class Meta:
@@ -65,3 +116,34 @@ class PostDelete(DeleteView):
     def get_success_url(self):
         messages.success(self.request, 'Post successfully deleted.')
         return reverse('posts')
+
+class PostUpdateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'image', 'description', 'lat', 'long']
+    
+
+
+
+# Auth
+class Signup(TemplateView):
+    template_name = "signup.html"
+
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            # write dtl to accept this message and auto display modal with warning inside after going back to submission page
+            messages.warning(self.request, 'Form submission error, plese try again.')
+            context = {"form": form}
+            return redirect("back")
+            return render(request, "registration/signup.html", context) # FIXME - if invalid form input, how to redirect back to modal form? Currently redirecting to unused signup page
