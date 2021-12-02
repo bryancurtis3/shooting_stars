a8fb801ce29b6fdf395f27d98699ad6d3fa35414
bryancurtis <curtispro97@gmail.com>
2021-12-02 15:55:58 -0800

    heroku changes

--- shooting_stars/settings.py
+++ shooting_stars/settings.py
@@ -143,3 +143,7 @@ AUTH_USER_MODEL = 'main_app.User'
 # LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
