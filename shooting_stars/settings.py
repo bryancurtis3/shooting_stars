1dfde540133a26a1cc9b4d73f7526c66624ad4fd
bryancurtis <curtispro97@gmail.com>
2021-12-04 08:24:20 -0800

    better logs

--- shooting_stars/settings.py
+++ shooting_stars/settings.py
@@ -144,6 +144,46 @@ AUTH_USER_MODEL = 'main_app.User'
 
 LOGOUT_REDIRECT_URL = '/'
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
